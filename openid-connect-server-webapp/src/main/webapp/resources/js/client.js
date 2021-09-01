5b37543eacbdb5499fdb48b85506bd0a913f5285
Dominik Frantisek Bucik <bucik@ics.muni.cz>
2021-09-01 08:51:06 +0200

    refactor: Remove stats service

--- openid-connect-server-webapp/src/main/webapp/resources/js/client.js
+++ openid-connect-server-webapp/src/main/webapp/resources/js/client.js
@@ -200,10 +200,6 @@ var RegistrationTokenModel = Backbone.Model.extend({
 	urlRoot: 'api/tokens/registration'
 });
 
-var ClientStatsModel = Backbone.Model.extend({
-	urlRoot: 'api/stats/byclientid'
-});
-
 var ClientCollection = Backbone.Collection.extend({
 
 	initialize: function() {
@@ -303,19 +299,12 @@ var ClientView = Backbone.View.extend({
 		});
 
 		this.updateMatched();
-		this.updateStats();
 
 		$(this.el).i18n();
 
 		this.isRendered = true;
 
 		return this;
-	},
-
-	updateStats: function(eventName) {
-		$('.count', this.el).html(this.countTemplate({
-			count: this.options.clientStat.get('approvedSiteCount')
-		}));
 	},
 
 	showRegistrationToken: function(e) {
@@ -471,8 +460,6 @@ var ClientListView = Backbone.View.extend({
 
 	tagName: 'span',
 
-	stats: {},
-
 	initialize: function(options) {
 		this.options = options;
 		this.filteredModel = this.model;
@@ -569,15 +556,6 @@ var ClientListView = Backbone.View.extend({
 		return this.views[index];
 	},
 
-	getStat: function(index) {
-		if (!this.stats[index]) {
-			this.stats[index] = new ClientStatsModel({
-				id: index
-			});
-		}
-		return this.stats[index];
-	},
-
 	togglePlaceholder: function() {
 		// set up pagination
 		var numPages = Math.ceil(this.filteredModel.length / 10);
@@ -637,17 +615,6 @@ var ClientListView = Backbone.View.extend({
 			} else {
 				if (!view.isRendered) {
 					view.render();
-					var clientStat = view.options.clientStat;
-
-					// load and display the stats
-					$.when(clientStat.fetchIfNeeded({
-						success: function(e) {
-
-						},
-						error: app.errorHandlerView.handleError()
-					})).done(function(e) {
-						view.updateStats();
-					});
 				}
 				$(view.el).show();
 			}
